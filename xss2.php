baa60e08972f9ba1c1717fd76549d0df837f999d
adrianmp <adrian.medina@bejattos.com>
2013-01-10 21:27:51 -0600

    despues de poner inputfilter permitiendo etiquetas

--- xss2.php
+++ xss2.php
@@ -1,6 +1,6 @@
 <?php
 	require_once("class.inputfilter.php");
-	$ifilter = new InputFilter();
+	$ifilter = new InputFilter(array('em', 'strong'));
 	$_POST = $ifilter->process($_POST);
 	echo $_POST['texto'];
 ?>
